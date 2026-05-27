4817852f7d5cf089e3ba6984b5dc38da1e5a5cc8
tutor <tutor@labsji.dev>
2026-05-27 10:55:27 +0000

    Add gradle wrapper + fix micrometer dependency (1.13.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.12.2")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.13.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("ch.qos.logback:logback-classic:1.4.14")
 }
